a3a9c1a9a0f2d49e2f48210a4df58cfe50652c97
S. Rasmussen <siras@outlook.com>
2017-12-12 13:11:04 +0100

    Update esp8266Client.ino

--- esp8266Client.ino
+++ esp8266Client.ino
@@ -6,88 +6,49 @@
  *
  */
 
+// https://gist.github.com/igrr/7f7e7973366fc01d6393 another reference
+
 #include <ESP8266WiFi.h>
 
 const char* ssid     = "your-ssid"; // what is this?
 const char* password = "your-password"; // and this?
 
-const char* host = "data.sparkfun.com"; // webservice I suppose
-const char* streamId   = "...................."; // what
-const char* privateKey = "...................."; // what
+const char* webService = "data.sparkfun.com"; 
 
 void setup() {
   Serial.begin(115200); // baud rate
   delay(10);
 
-  // We start by connecting to a WiFi network
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  // Connect to a WiFi network
   
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA); // what
   WiFi.begin(ssid, password);
   
+  /* while it is not connected print a '.' */
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("");
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-int value = 0;
-
 void loop() {
   delay(5000);
-  ++value;
 
-  Serial.print("connecting to ");
-  Serial.println(host);
+  Serial.print("Connecting to ");
+  Serial.println(webService);
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
   if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
+    Serial.println("Connection failed");
     return;
   }
-  
-  // We now create a URI for the request
-  String url = "/input/";
-  url += streamId;
-  url += "?private_key=";
-  url += privateKey;
-  url += "&value=";
-  url += value;
-  
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-  
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
-  }
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("closing connection");
 }
